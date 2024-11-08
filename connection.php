a131665ed2f0eea4cebc7f3fd96eb1ffa6f1b155
AsmaEva21 <110212541+AsmaEva21@users.noreply.github.com>
2024-11-08 18:54:35 +0600

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$dbName = "bank_management_system";
+$dbUsername = "root";
+$dbpassword = "";
+
+
+$con = mysqli_connect($host,$dbUsername,$dbpassword,$dbName);
+
+
+
+
+
